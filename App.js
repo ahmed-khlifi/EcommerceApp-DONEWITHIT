b89b33de50c2087bd980298e2df7c0341bde707b
ahmed khelifi <ahmedkh3lifi@gmail.com>
2022-02-16 15:12:02 +0100

    add navigation stack between Home, Login and Register screens

--- App.js
+++ App.js
@@ -1,23 +1,41 @@
 import React, { useState } from "react";
 import { Button, Text } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import Screen from "./app/components/Screen";
+import AuthNavigator from "./app/navigation/AuthNavigator";
+
+const Link = () => {
+  const navigation = useNavigation();
+  return (
+    <Button title="Go to tweet" onPress={() => navigation.navigate("Tweets")} />
+  );
+};
 
 const Tweets = ({ navigation }) => (
   <Screen>
     <Text>Teweets</Text>
     <Button
       title="view tweet"
-      onPress={() => navigation.navigate("TweetDetails")}
+      onPress={() => navigation.navigate("TweetDetails", { id: 5 })}
     />
   </Screen>
 );
 
-const TweetDetails = () => (
+const TweetDetails = ({ route }) => (
   <Screen>
     <Text>Tweet details</Text>
+    <Text>The id is : {route.params.id}</Text>
+    <Link />
+  </Screen>
+);
+
+const Account = () => (
+  <Screen>
+    <Text>Account</Text>
   </Screen>
 );
 
@@ -30,6 +48,29 @@ const StackNavigator = () => (
   </Stack.Navigator>
 );
 
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => (
+  <Tab.Navigator
+    tabBarOptions={{
+      activeBackgroundColor: "tomato",
+      activeTintColor: "white",
+      inactiveBackgroundColor: "#eee",
+      inactiveTintColor: "black",
+    }}
+  >
+    <Tab.Screen
+      name="Feed"
+      component={Tweets}
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <MaterialCommunityIcons name="home" size={size} color={color} />
+        ),
+      }}
+    />
+    <Tab.Screen name="Account" component={Account} />
+  </Tab.Navigator>
+);
+
 export default function App() {
   console.log("====================================");
   console.log("starting the app");
@@ -38,7 +79,7 @@ export default function App() {
   return (
     <Screen>
       <NavigationContainer>
-        <StackNavigator />
+        <AuthNavigator />
       </NavigationContainer>
     </Screen>
   );
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -2,20 +2,24 @@ import React from "react";
 import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
 import AppButton from "../components/AppButton";
 
-function WelcomeScreen(props) {
+function WelcomeScreen({ navigation }) {
   return (
     <ImageBackground
       blurRadius={10}
       style={styles.background}
-      source={require("../assets/background.jpg")}
+      source={require("../assets/couch.jpg")}
     >
       <View style={styles.logoContainer}>
         <Image style={styles.logo} source={require("../assets/logo-red.png")} />
         <Text style={styles.tagline}>Sell What You Don't Need</Text>
       </View>
       <View style={styles.buttonsContainer}>
-        <AppButton title="Login" />
-        <AppButton title="Register" color="secondary" />
+        <AppButton title="Login" onPress={() => navigation.navigate("Login")} />
+        <AppButton
+          title="Register"
+          color="secondary"
+          onPress={() => navigation.navigate("Register")}
+        />
       </View>
     </ImageBackground>
   );
