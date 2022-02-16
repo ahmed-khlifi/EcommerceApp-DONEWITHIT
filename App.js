8d464aaf53c5d436b01a3e9120af63864aaf364e
ahmed khelifi <ahmedkh3lifi@gmail.com>
2022-02-16 11:49:42 +0100

    add navigation

--- App.js
+++ App.js
@@ -1,28 +1,45 @@
 import React, { useState } from "react";
+import { Button, Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
 import Screen from "./app/components/Screen";
-import ImageInput from "./app/components/ImageInput";
-import ImageInputList from "./app/components/ImageInputList";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+
+const Tweets = ({ navigation }) => (
+  <Screen>
+    <Text>Teweets</Text>
+    <Button
+      title="view tweet"
+      onPress={() => navigation.navigate("TweetDetails")}
+    />
+  </Screen>
+);
+
+const TweetDetails = () => (
+  <Screen>
+    <Text>Tweet details</Text>
+  </Screen>
+);
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen name="TweetDetails" component={TweetDetails} />
+  </Stack.Navigator>
+);
 
 export default function App() {
   console.log("====================================");
   console.log("starting the app");
   console.log("====================================");
 
-  const [imageUris, setImageUris] = useState([]);
-
-  const handleAdd = (uri) => {
-    setImageUris([...imageUris, uri]);
-  };
-
-  const handleRemove = (uri) => {
-    setImageUris(imageUris.filter((img) => img !== uri));
-  };
-
   return (
     <Screen>
-      <ListingEditScreen />
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
     </Screen>
   );
 }
